567fc93ee312ebc326b923f57b8071ec70244806
Cee <18570103+CeeIM@users.noreply.github.com>
2023-07-03 22:39:43 +0800

    Update ols-install.sh

--- shell/ols-install.sh
+++ shell/ols-install.sh
@@ -1,63 +1,62 @@
 #!/bin/bash
+# Author: Cee
+# Website: https://cee.im
 
 start_time=$(date +%s)
 
-# 隐藏具体输出，只显示安装步骤或报错，并具有等待标识
 function display_message() {
   echo -e "\n>> $1"
   echo -e "----------------------------------------\n"
 }
 
-# 获取OLS版本号
 display_message "正在获取 OpenLiteSpeed 版本号"
 OLS_VER=$(curl -s https://cyberpanel.sh/openlitespeed.org/packages/release)
 display_message "OpenLiteSpeed 最新版本号: $OLS_VER"
 
-# 获取服务器 IP 和国家
 display_message "正在获取服务器 IP 和地区"
 IP=$(curl -s ipinfo.io/ip)
 COUNTRY=$(curl -s ipinfo.io/country)
 
-# 根据国家选择安装方式和下载链接
 if [ "$COUNTRY" = "CN" ]; then
   display_message "服务器地区为（$COUNTRY），将使用加速软件源安装 OpenLiteSpeed"
   display_message "正在安装软件源"
-  curl -sS http://cyberpanel.sh/rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | bash
+  curl -sS http://cyberpanel.sh/rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | bash >/dev/null
   sudo sed -i 's/rpms.litespeedtech.com/cyberpanel.sh\/rpms.litespeedtech.com/' /etc/apt/sources.list.d/lst_debian_repo.list
-  sudo apt-get update -y
+  sudo apt-get update -y >/dev/null
   display_message "正在下载 OpenLiteSpeed 安装包"
-  wget -q "https://cyberpanel.sh/openlitespeed.org/packages/openlitespeed-${OLS_VER}.tgz"
+  wget -q "https://cyberpanel.sh/openlitespeed.org/packages/openlitespeed-${OLS_VER}.tgz" -P ~/
 else
   display_message "服务器地区为（$COUNTRY），将使用官方软件源安装 OpenLiteSpeed"
   display_message "正在安装软件源"
-  curl -sS http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | bash
+  curl -sS http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | bash >/dev/null
   display_message "正在下载 OpenLiteSpeed 安装包"
-  wget -q "https://openlitespeed.org/packages/openlitespeed-${OLS_VER}.tgz"
+  wget -q "https://openlitespeed.org/packages/openlitespeed-${OLS_VER}.tgz" -P ~/
 fi
 
-# 安装 OpenLiteSpeed
 display_message "正在安装 OpenLiteSpeed"
-tar xf "openlitespeed-${OLS_VER}.tgz"
-cd "openlitespeed*"
+tar xf "${HOME}/openlitespeed-${OLS_VER}.tgz"
+cd "${HOME}/openlitespeed"
 sudo ./install.sh
 
-# 使用软件源安装 lsphp74 lsphp74-* lsphp81 lsphp81-* lsphp82 lsphp82-*
 display_message "正在安装 LSPHP"
-sudo apt-get install -y lsphp74 lsphp74-* lsphp81 lsphp81-* lsphp82 lsphp82-*
+sudo apt-get install -y lsphp74 lsphp74-* lsphp81 lsphp81-* lsphp82 lsphp82-* >/dev/null
 
-# 打印控制台地址和默认用户名密码
 display_message "OpenLiteSpeed 安装完成"
-echo -e "控制台地址: http://$IP:7080"
+echo -e "控制台地址: https://$IP:7080"
+sudo cat /usr/local/lsws/adminpasswd
 
 end_time=$(date +%s)
 duration=$((end_time - start_time))
+hours=$((duration / 3600))
+minutes=$(( (duration % 3600) / 60 ))
+seconds=$((duration % 60))
 
-# 询问是否更改默认密码
-read -p "是否更改默认密码？(y/n): " change_password
+read -p "是否更改默认用户名/密码？(y/n): " change_password
 if [ "$change_password" = "y" ] || [ "$change_password" = "Y" ]; then
   sudo /usr/local/lsws/admin/misc/admpass.sh
 fi
+sudo systemctl restart lsws
 
-# 显示总用时
-display_message "全部设置已完成"
-echo -e "\n脚本总用时：$((duration / 60)) 分钟"
+display_message "安装总用时：$hours 小时 $minutes 分钟 $seconds 秒"
+
+rm -rf ${HOME}/openlitespeed*
